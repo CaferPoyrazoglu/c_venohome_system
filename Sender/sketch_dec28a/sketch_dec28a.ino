d3be591bbd2a1a916aa30569469550188132e8ad
Cafer <cafererhan@gmail.com>
2021-12-29 13:36:31 +0300

    first commit

---
+++ Sender/sketch_dec28a/sketch_dec28a.ino
@@ -0,0 +1,75 @@
+#include <espnow.h>
+#include <ESP8266WiFi.h>
+
+#define SENDER_ID 1
+
+uint8_t broadcastAddress[] = {0x3C, 0x61, 0x05, 0xE4, 0xA8, 0x4F};
+
+typedef struct struct_message {
+    int id;
+    float temp;
+    float hum;
+    int readingId;
+} struct_message;
+
+struct_message myData;
+
+unsigned long previousMillis = 0;
+const long interval = 10000;        
+
+unsigned int readingId = 0;
+
+constexpr char WIFI_SSID[] = "CAFER";
+
+int32_t getWiFiChannel(const char *ssid) {
+  if (int32_t n = WiFi.scanNetworks()) {
+    for (uint8_t i=0; i<n; i++) {
+      if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+        return WiFi.channel(i);
+      }
+    }
+  }
+  return 0;
+}
+
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Son Gonderilen Paket Durumu: ");
+  if (sendStatus == 0){
+    Serial.println("Aktarma Basarili!");
+  }
+  else{
+    Serial.println("Aktarma Hatasi!");
+  }
+}
+ 
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  int32_t channel = getWiFiChannel(WIFI_SSID);
+  WiFi.printDiag(Serial); 
+  wifi_promiscuous_enable(1);
+  wifi_set_channel(channel);
+  wifi_promiscuous_enable(0);
+  WiFi.printDiag(Serial);
+
+  if (esp_now_init() != 0) {
+    Serial.println("ESP-NOW iletisimi basarisiz!");
+    return;
+  }
+  
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+}
+ 
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    myData.id = SENDER_ID;
+    myData.temp = ((int)random(0, 40));
+    myData.hum = ((int)random(0, 100));
+    myData.readingId = readingId++;
+    esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  }
+}
