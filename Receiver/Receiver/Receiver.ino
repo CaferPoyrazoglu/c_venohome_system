301b80869a9d6a08afcdf6accb01099d59bf0457
Cafer <cafererhan@gmail.com>
2021-12-29 16:26:57 +0300

    stb

--- Receiver/Receiver/Receiver.ino
+++ Receiver/Receiver/Receiver.ino
@@ -1,3 +1,49 @@
+#include <espnow.h>
+#include <ESP8266WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "ESPAsyncTCP.h"
+#include <Arduino_JSON.h>
+
+const char* ssid = "BSC";
+const char* password = "32bsc2021";
+
+typedef struct struct_message {
+  int id;
+  float temp;
+  float hum;
+  unsigned int readingId;
+} struct_message;
+
+struct_message incomingReadings;
+
+JSONVar board;
+
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
+void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) { 
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  
+  board["id"] = incomingReadings.id;
+  board["temperature"] = incomingReadings.temp;
+  board["humidity"] = incomingReadings.hum;
+  board["readingId"] = String(incomingReadings.readingId);
+  String jsonString = JSON.stringify(board);
+  events.send(jsonString.c_str(), "new_readings", millis());
+  
+  Serial.printf("Board ID %u: %u bytes\n", incomingReadings.id, len);
+  Serial.printf("t value: %4.2f \n", incomingReadings.temp);
+  Serial.printf("h value: %4.2f \n", incomingReadings.hum);
+  Serial.printf("readingID value: %d \n", incomingReadings.readingId);
+  Serial.println();
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
 <html>
 
@@ -52,8 +98,9 @@
         }
 
         .timestamp {
-            color: #bebebe;
+            color: #000000;
             font-size: 1rem;
+            font-weight: bold;
         }
 
         .card-title {
@@ -138,13 +185,17 @@
     <div class="content">
         <div class="cards">
             <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Sıcaklık</p>
-                <p><span class="reading"><span id="s1"></span> &deg;C</span></p>
+                <p class="card-title"><i class="fas fa-thermometer-half"></i> Sicaklik</p>
+                <p><span class="reading"><span id="t1"></span> &deg;C</span></p>
+                <p class="timestamp">Son Okunma Zamani</p>
+                <p class="timestamp"><span id="d1"></span></p>
 
             </div>
             <div class="card temperature">
                 <p class="card-title"><i class="fas fa-thermometer-half"></i> Nem</p>
-                <p><span class="reading"><span id="n1"></span> &deg;%</span></p>
+                <p><span class="reading"><span id="h1"></span> &deg;%</span></p>
+                <p class="timestamp">Son Okunma Zamani</p>
+                <p class="timestamp"><span id="d2"></span></p>
 
             </div>
             <div class="card temperature">
@@ -165,7 +216,7 @@
             </div>
 
             <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Röle - 1</p>
+                <p class="card-title"><i class="fas fa-thermometer-half"></i> Role - 1</p>
                 <p><span class="reading"><span id="rly1"></span> <label class="switch">
                             <input type="checkbox">
                             <span class="slider round"></span>
@@ -173,7 +224,7 @@
 
             </div>
             <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Röle - 2</p>
+                <p class="card-title"><i class="fas fa-thermometer-half"></i> Role - 2</p>
                 <p><span class="reading"><span id="rly2"></span> <label class="switch">
                             <input type="checkbox">
                             <span class="slider round"></span>
@@ -184,6 +235,16 @@
         </div>
     </div>
     <script>
+        function getDateTime() {
+            var currentdate = new Date();
+            var datetime = currentdate.getDate() + "/" +
+                (currentdate.getMonth() + 1) + "/" +
+                currentdate.getFullYear() + " at " +
+                currentdate.getHours() + ":" +
+                currentdate.getMinutes() + ":" +
+                currentdate.getSeconds();
+            return datetime;
+        }
         if (!!window.EventSource) {
             var source = new EventSource('/events');
 
@@ -203,12 +264,58 @@
             source.addEventListener('new_readings', function (e) {
                 console.log("new_readings", e.data);
                 var obj = JSON.parse(e.data);
-                document.getElementById("t" + obj.id).innerHTML = obj.temperature.toFixed(2);
-                document.getElementById("h" + obj.id).innerHTML = obj.humidity.toFixed(2);
+                document.getElementById("t1").innerHTML = obj.temperature.toFixed(1);
+                document.getElementById("h1").innerHTML = obj.humidity.toFixed(1);
+                document.getElementById("d1").innerHTML = getDateTime();
+                document.getElementById("d2").innerHTML = getDateTime();
 
             }, false);
         }
     </script>
 </body>
 
-</html>
+</html>)rawliteral";
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Setting as a Wi-Fi Station..");
+  }
+  Serial.print("Station IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Wi-Fi Channel: ");
+  Serial.println(WiFi.channel());
+
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  esp_now_register_recv_cb(OnDataRecv);
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+   
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+ 
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+  server.begin();
+}
+ 
+void loop() {
+  static unsigned long lastEventTime = millis();
+  static const unsigned long EVENT_INTERVAL_MS = 5000;
+  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
+    events.send("ping",NULL,millis());
+    lastEventTime = millis();
+  }
+}
