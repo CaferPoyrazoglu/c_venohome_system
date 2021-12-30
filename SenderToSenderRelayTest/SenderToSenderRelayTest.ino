0c8bc34ec4a1a7924c6b8dca1500bb92c18306e8
Cafer <cafererhan@gmail.com>
2021-12-30 13:41:02 +0300

    SenderToSender got com. but sender relay got problems

---
+++ SenderToSenderRelayTest/SenderToSenderRelayTest.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+uint8_t broadcastAddress[] = {0xC4, 0x5B, 0xBE, 0x67, 0x9C, 0xEC};
+
+typedef struct message  {
+ int relay1;
+} message ;
+
+message  myData;
+
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 2000; 
+
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Last Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(9600);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+}
+ 
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    // Set values to send
+    myData.relay1 = 0;
+
+    // Send message via ESP-NOW
+    esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+    lastTime = millis();
+  }
+}
