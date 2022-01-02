17f778e897604f175cfaf76d5eb1dc0fb5349b48
Cafer <cafererhan@gmail.com>
2022-01-02 19:13:08 +0300

    ui fix

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -46,36 +46,50 @@ void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
 <html>
+
 <head>
     <title>Small Paket Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Varela+Round" />
     <link rel="icon" href="data:,">
     <style>
         html {
-            font-family: Arial;
+            font-family: Futura, "Trebuchet MS", Arial, sans-serif;
+            font-size: 20px;
+            font-style: normal;
+            font-variant: normal;
+            font-weight: 500;
+            line-height: 26.4px;
             display: inline-block;
             text-align: center;
         }
+
         h1 {
             font-size: 2rem;
         }
+
         body {
             margin: 0;
         }
+
         .topnav {
             overflow: hidden;
             background-color: #2f4468;
             color: white;
             font-size: 1.7rem;
         }
+
         .content {
             padding: 20px;
         }
+
         .card {
             background-color: rgb(255, 255, 255);
-            box-shadow: 2px 2px 12px 1px rgba(0, 0, 0, 0.5);
+            box-shadow: 2px 2px 24px 1px rgb(180, 180, 180);
             border-radius: 16px;
+
         }
+
         .cards {
             max-width: 700px;
             margin: 0 auto;
@@ -83,36 +97,45 @@ const char index_html[] PROGMEM = R"rawliteral(
             grid-gap: 2rem;
             grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
         }
+
         .reading {
-            font-size: 2.8rem;
+            font-size: 36px;
+            font-weight: 500;
         }
+
         .timestamp {
             color: #000000;
-            font-size: 1rem;
-            font-weight: bold;
+            font-size: 16px;
+            font-weight: 400;
         }
+
         .card-title {
-            font-size: 1.2rem;
+            font-size: 22px;
             font-weight: bold;
         }
+
         .card.temperature {
             color: #000000;
         }
+
         .card.humidity {
             color: #50B8B4;
         }
+
         .switch {
             position: relative;
             display: inline-block;
             width: 60px;
             height: 34px;
         }
+
         /* Hide default HTML checkbox */
         .switch input {
             opacity: 0;
             width: 0;
             height: 0;
         }
+
         /* The slider */
         .slider {
             position: absolute;
@@ -125,6 +148,7 @@ const char index_html[] PROGMEM = R"rawliteral(
             -webkit-transition: .4s;
             transition: .4s;
         }
+
         .slider:before {
             position: absolute;
             content: "";
@@ -136,69 +160,57 @@ const char index_html[] PROGMEM = R"rawliteral(
             -webkit-transition: .4s;
             transition: .4s;
         }
+
         input:checked+.slider {
             background-color: #2196F3;
         }
+
         input:focus+.slider {
             box-shadow: 0 0 1px #2196F3;
         }
+
         input:checked+.slider:before {
             -webkit-transform: translateX(26px);
             -ms-transform: translateX(26px);
             transform: translateX(26px);
         }
+
         /* Rounded sliders */
         .slider.round {
             border-radius: 34px;
         }
+
         .slider.round:before {
             border-radius: 50%;
         }
     </style>
 </head>
-<body style="background-color: rgb(59, 124, 72);">
+
+<body style="background-color: rgb(231, 231, 231);">
     <div class="content">
         <div class="cards">
             <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Sicaklik</p>
+                <p class="card-title"><i class="fas fa-thermometer-half"></i> Sıcaklık</p>
                 <p><span class="reading"><span id="t1"></span> &deg;C</span></p>
-                <p class="timestamp">Son Okunma Zamani</p>
+                <p class="timestamp">Son Okunma Zamanı</p>
                 <p class="timestamp"><span id="d1"></span></p>
             </div>
             <div class="card temperature">
                 <p class="card-title"><i class="fas fa-thermometer-half"></i> Nem</p>
                 <p><span class="reading"><span id="h1"></span> &deg;%</span></p>
-                <p class="timestamp">Son Okunma Zamani</p>
+                <p class="timestamp">Son Okunma Zamanı</p>
                 <p class="timestamp"><span id="d2"></span></p>
             </div>
+
+
             <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Su Kesici</p>
-                <p><span class="reading"><span id="sk1"></span> <label class="switch">
-                            <input type="checkbox">
-                            <span class="slider round"></span>
-                        </label></span></p>
-            </div>
-            <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Gaz Kesici</p>
-                <p><span class="reading"><span id="gk1"></span> <label class="switch">
-                            <input type="checkbox">
-                            <span class="slider round"></span>
-                        </label></span></p>
-            </div>
-            <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Role - 1</p>
+                <p class="card-title"><i class="fas fa-thermometer-half"></i> Röle - 1</p>
                 <p><span class="reading"><span id="rly1"></span> <label class="switch">
                             <input type="checkbox">
                             <span class="slider round"></span>
                         </label></span></p>
             </div>
-            <div class="card temperature">
-                <p class="card-title"><i class="fas fa-thermometer-half"></i> Role - 2</p>
-                <p><span class="reading"><span id="rly2"></span> <label class="switch">
-                            <input type="checkbox">
-                            <span class="slider round"></span>
-                        </label></span></p>
-            </div>
+
         </div>
     </div>
     <script>
@@ -236,6 +248,7 @@ const char index_html[] PROGMEM = R"rawliteral(
         }
     </script>
 </body>
+
 </html>)rawliteral";
 
 void setup() {
