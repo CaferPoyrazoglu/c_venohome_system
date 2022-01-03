6cb98fef069a333f0276f6aa9691b8d18294b500
Cafer <cafererhan@gmail.com>
2022-01-03 16:37:23 +0300

    Relay and RelaysSender updated

--- SenderRelay/SenderRelay.ino
+++ SenderRelay/SenderRelay.ino
@@ -2,13 +2,25 @@
 #include <espnow.h>
 #define RELAY 0 
 
+uint8_t broadcastAddress[] = {0x3C, 0x61, 0x05, 0xE4, 0xA8, 0x4F};
+int role1_Durumu = 0;
+
 typedef struct message {
     int role1;
 } message;
 
 message msg;
 
-int role1_Durumu = 0;
+
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Last Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
 
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&msg, incomingData, sizeof(msg));
@@ -18,22 +30,25 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   Serial.println();
 
 }
+
  
 void setup() {
   Serial.begin(9600);
   pinMode(RELAY,OUTPUT);
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();        
+  WiFi.disconnect();
 
-  if(esp_now_init() != 0) {
-    Serial.println("ESP-NOW Baglantisi basarisiz!");
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
     return;
   }
 
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
   esp_now_register_recv_cb(OnDataRecv);
-  Serial.println("Basarili!");
 }
-
+ 
 void loop() {
   if(role1_Durumu == 1)
   {
--- SenderToSenderRelayTest/SenderToSenderRelayTest.ino
+++ SenderToSenderRelayTest/SenderToSenderRelayTest.ino
@@ -9,49 +9,41 @@ typedef struct message  {
 
 message  myData;
 
-unsigned long lastTime = 0;  
-unsigned long timerDelay = 2000; 
+int temp = 0;
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
-  Serial.print("Last Packet Send Status: ");
+  Serial.print("Durum: ");
   if (sendStatus == 0){
-    Serial.println("Delivery success");
+    Serial.println("Aktarma Basarili");
   }
   else{
-    Serial.println("Delivery fail");
+    Serial.println("Aktarma Hatasi!");
   }
 }
  
 void setup() {
-  // Init Serial Monitor
   Serial.begin(9600);
  
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != 0) {
-    Serial.println("Error initializing ESP-NOW");
+    Serial.println("ESP-NOW Hatasi!");
     return;
   }
-
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
+  
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_send_cb(OnDataSent);
-  
-  // Register peer
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
  
 void loop() {
-  if ((millis() - lastTime) > timerDelay) {
-    // Set values to send
-    myData.relay1 = ((int)random(0, 2));;
-
-    // Send message via ESP-NOW
+    if(temp == 2){
+      temp = 0;
+    }
+    
+    myData.relay1 = temp;
+    temp++;
     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
-    lastTime = millis();
+    
+    delay(2000);
   }
-}
